1ca8d2b170c03832018aa8bcd955195d405d95ec
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-03 13:06:28 +0200

    Started the Solution CourtBooking Added all the projects.

---
+++ CourtBooking.ManualTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourtBooking.ManualTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CourtBooking.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CourtBooking.Service
+{
+    public class Class1
+    {
+    }
+}
