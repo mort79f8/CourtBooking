85336248a444cea0e54a71d09f14902cd236bcaa
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-03 13:13:56 +0200

    Created the propeties for Entities

--- CourtBooking.Entities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CourtBooking.Entities
-{
-    public class Class1
-    {
-    }
-}
---
+++ CourtBooking.Entities/Entities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourtBooking.Entities
+{
+    class Entities
+    {
+        public int Id { get; set; }
+        public int Courtnumber { get; set; }
+        public DateTime BookingTime { get; set; }
+        public string Booker { get; set; }
+
+    }
+}
